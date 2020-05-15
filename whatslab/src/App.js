19db6c0b8429b8135e52a1d8fb9bbde3c589d007
yuzokamoto <yuzokamoto@gmail.com>
2020-05-15 16:13:18 -0300

    Adicionado entrega obrigatória codada em conjunto no CodeSandbox

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+state = {
+  mensagensEnviadas: [
+  {
+  usuario: "",
+  mensagem: ""
+  } 
+], 
+  valorInputUsuario: "",
+  valorInputMensagem: ""
+};
+
+onChangeInputUsuario = (event) => {
+  this.setState ({valorInputUsuario: event.target.value})
+}
+
+onChangeInputMensagem = (event) => {
+  this.setState ({valorInputMensagem: event.target.value})
+}
+
+onClickMandaMensagem = () => {
+  const novaMensagem = {
+    mensagem: this.state.valorInputMensagem,
+    usuario: this.state.valorInputUsuario
+  }
+  const novasMensagens = [...this.state.mensagensEnviadas, novaMensagem];
+  this.setState ({
+    mensagensEnviadas: novasMensagens,
+    valorInputMensagem: ""
+  })
+}
+    
+onKeyDownEnter = (event) => {
+  if (event.key === "Enter") {
+    this.onClickMandaMensagem()
+  }
+}
+
+
+  render() {
+
+    const listaDeMensagens = this.state.mensagensEnviadas.map((mensagem) => {
+      
+        return (
+        <div>
+          <span>
+            <strong>{mensagem.usuario}</strong>: {mensagem.mensagem} 
+          </span> 
+        </div>
+        )
+    });
+        
+
+    return (
+
+      <div id="App">
+
+        <div className="containerDeMensagem"> 
+          {listaDeMensagens}
+        </div>
+        
+        <div className="containerDeInput">
+              
+            <input placeholder={"Usuário"} 
+            value={this.state.valorInputUsuario} 
+            onChange={this.onChangeInputUsuario}
+            />
+
+            <input placeholder={"Mensagem"} 
+            value={this.state.valorInputMensagem}
+            onChange={this.onChangeInputMensagem}
+            onKeyDown={this.onKeyDownEnter} 
+            />
+
+            <button onClick={this.onClickMandaMensagem}
+            >              
+            Enviar 
+            </button>
+       </div> 
+       
+     </div>   
+    );
+  }
 }
 
 export default App;
